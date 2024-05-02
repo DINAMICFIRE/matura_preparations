179808bf2bc55eb68e65f8a135a0918231359657
DINAMICFIRE <123200170+DINAMICFIRE@users.noreply.github.com>
2024-05-02 23:38:59 +0300

    soft uni exercise simulating voina card game

---
+++ matura_preparations/zad_pomagalo/src/zad_pomagalo_softuni/Voina.java
@@ -0,0 +1,48 @@
+package zad_pomagalo_softuni;
+
+public class Voina {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        Set<Integer> firstPlayer = new LinkedHashSet<>();
+        Set<Integer> secondPlayer = new LinkedHashSet<>();
+        int[] input = Arrays.stream(scan.nextLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < input.length; i++) {
+            firstPlayer.add(input[i]);
+        }
+        input = Arrays.stream(scan.nextLine().split("\\s")).mapToInt(Integer::parseInt).toArray();
+        for (int i = 0; i < input.length; i++) {
+            secondPlayer.add(input[i]);
+        }
+        int steps = 0;
+        while (!firstPlayer.isEmpty() && !secondPlayer.isEmpty()){
+            if (steps >= 50){
+                break;
+            }
+            Integer firstNum = firstPlayer.iterator().next();
+            Integer secondNum = secondPlayer.iterator().next();
+            firstPlayer.remove(firstNum);
+            secondPlayer.remove(secondNum);
+            if (firstNum >= secondNum){
+                
+                firstPlayer.add(firstNum);
+                firstPlayer.add(secondNum);
+
+            }else{
+                secondPlayer.add(firstNum);
+                secondPlayer.add(secondNum);
+
+            }
+            steps++;
+        }
+        if (firstPlayer.size() > secondPlayer.size()){
+            System.out.println("First player win!");
+        }else if (firstPlayer.size() < secondPlayer.size()){
+            System.out.println("Second player win!");
+        }else{
+            System.out.println("Draw!");
+        }
+
+	}
+
+}
