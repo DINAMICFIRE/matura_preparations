c87a1bd6d913233d2f280864fa7ae08a7a448e07
DINAMICFIRE <friv9friv9@gmail.com>
2024-05-20 22:11:20 +0300

    wanted to check if matura zad is right

---
+++ matura_preparations/zad_pomagalo/src/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		String a = "1";
+		String b = "2";
+		 Integer.parseInt(a.getText())*Integer.parseInt(b.getText());
+		 //must be gui
+	}
+
+}
